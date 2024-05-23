99d306701abc8a994d72a2b808f590772b72c8b9
reginayuju <ytai5193@gmail.com>
2024-05-23 16:57:42 +0800

    bubblesort練習

--- Problems/bubblesort.py
+++ Problems/bubblesort.py
@@ -4,15 +4,23 @@ Bubble sort practice
 '''
 import numpy as np
 
+sorted_array = []
+
 
 def bubble_sort(arr):
-
+    for j in range(len(arr) - 1):
+        for i in range(len(arr) - 1):
+            if (arr[i]) > (arr[i + 1]):
+                b = arr[i]
+                arr[i] = arr[i + 1]
+                arr[i + 1] = b
+    print(arr)
     return arr
 
 
 if __name__ == '__main__':
-    np.random.seed(0)
-    array = np.random.randint(0, 100, 10)
+
+    array = np.random.randint(0, 100, 20)
     print('Original array:', array)
 
     sorted_array = bubble_sort(array)
