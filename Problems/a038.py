2b8930d21afc1708c22bc48a5c451279326f6e20
reginayuju <ytai5193@gmail.com>
2024-05-30 11:31:13 +0800

    a038完成

---
+++ Problems/a038.py
@@ -0,0 +1,15 @@
+# 方法1
+n = input()
+
+b = n[-1::-1]
+print(int(b))
+
+# n [開始:結束:公差]
+# n[-1::]
+
+# 方法2
+n = input()
+for i in range(-1, -1 * (len(n) + 1), -1):
+    if n[i] != "0":
+        break
+print(n[i::-1])
