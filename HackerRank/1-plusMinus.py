7a056de2016621ae0e7c505fb4b1416ca96c29a1
reginayuju <ytai5193@gmail.com>
2024-06-07 14:29:24 +0800

    1完成

--- HackerRank/1-plusMinus.py
+++ HackerRank/1-plusMinus.py
@@ -12,13 +12,33 @@ import sys
 # The function accepts INTEGER_ARRAY arr as parameter.
 #
 
-def plusMinus(arr):
-    n = int(input())
-    arr=map(int, input().split())
+def plusMinus(n, arr):
+    p_count = 0
+    n_count = 0
+    z_count = 0
+    for x in (arr):
+        # print("ZZZ",x)
+    
+        if x > 0:
+            p_count = p_count + 1
+    
+        if x < 0:
+            n_count = n_count + 1
+            # print("NN", n_count)
+    
+        if x == 0:
+            z_count = z_count + 1
+    # print("AAA", p_count, p_count/n)
+    # print("BBB", n_count, n_count/n)
+    # print("CCC", z_count, z_count/n)
+    print('%.6f' %(p_count/n))
+    print('%.6f' %(n_count/n))
+    print('%.6f' %(z_count/n))
+        
 
 if __name__ == '__main__':
     n = int(input().strip())
 
     arr = list(map(int, input().rstrip().split()))
 
-    plusMinus(arr)
+    plusMinus(n, arr)
