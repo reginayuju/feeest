2e4c1cec2d4aff7cd9c2de426b48a3ccffa600c4
reginayuju <ytai5193@gmail.com>
2024-06-20 12:39:45 +0800

    No.4完成

--- Problems/Breaking the Records.py
+++ Problems/Breaking the Records.py
@@ -13,23 +13,36 @@ import sys
 # The function accepts INTEGER_ARRAY scores as parameter.
 #
 
+
 def breakingRecords(scores):
-    Min=[]
-    Max=[]
-    lo=0
-    hi=0
-    for i in range (len())
+    Min = scores[0]
+    Max = scores[0]
+    lo = 0
+    hi = 0
+    for i in range(1, len(scores)):
+        print("Game", i)
+        print("Score", scores[i])
+
+        if scores[i] < Min:
+            lo += 1
+            Min = scores[i]
+        if scores[i] > Max:
+            hi += 1
+            Max = scores[i]
+        print(Max, Min)
+        print(hi, lo)
+
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    # fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
-    n = int(input().strip())
+    # n = int(input().strip())
 
     scores = list(map(int, input().rstrip().split()))
 
     result = breakingRecords(scores)
 
-    fptr.write(' '.join(map(str, result)))
-    fptr.write('\n')
+    # fptr.write(' '.join(map(str, result)))
+    # fptr.write('\n')
 
-    fptr.close()
+    # fptr.close()
