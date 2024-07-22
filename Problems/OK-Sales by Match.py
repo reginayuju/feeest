15f433a6111ea6c6da1f686dda9da51b745242c6
reginayuju <ytai5193@gmail.com>
2024-07-22 16:43:46 +0800

    DONE

---
+++ Problems/OK-Sales by Match.py
@@ -0,0 +1,44 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+# def sockMerchant(n, ar):
+n = 100
+ar = [44, 55, 11, 15, 4, 72, 26, 91, 80, 14, 43, 78, 70, 75, 36, 83, 78, 91, 17, 17, 54, 65, 60, 21, 58, 98, 87, 45, 75, 97, 81, 18, 51, 43, 84, 54, 66, 10, 44, 45, 23, 38, 22, 44, 65, 9, 78, 42, 100, 94, 58, 5, 11, 69, 26, 20, 19, 64, 64, 93, 60, 96, 10, 10, 39, 94, 15, 4, 3, 10, 1, 77, 48, 74, 20, 12, 83, 97, 5, 82, 43, 15, 86, 5, 35, 63, 24, 53, 27, 87, 45, 38, 34, 7, 48, 24, 100, 14, 80, 54]
+ans= []
+match_list =[]
+n_list= list(range(0, 101))
+print(n_list)
+for i in range (len(n_list)):
+    print(i)
+    x = ar.count(n_list[i])
+    ans.append(x)
+# print(ans)
+for j in range (len(ans)):
+    match_list.append(ans[j]//2)
+print(sum(match_list))
+
+
+# if __name__ == '__main__':
+#     fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+#     n = int(input().strip())
+
+#     ar = list(map(int, input().rstrip().split()))
+
+#     result = sockMerchant(n, ar)
+
+#     fptr.write(str(result) + '\n')
+
+#     fptr.close()
